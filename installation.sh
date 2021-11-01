ad71e9d2e9cf3070bc688a92a7076a4dd1f47a57
Carlos Arriaga <carlos.arriaga.prieto@alumnos.upm.es>
2021-11-01 13:47:48 +0100

    Init predcitor with spark submit

--- installation.sh
+++ installation.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 
-sudo apt-get update
+usage(){
 
-sudo apt-get install -y git
-git clone https://github.com/ging/practica_big_data_2019
+cat << EOF
+OPTIONS:
+   -h      Show this message
+   -d      Install docker
+   -c      Install docker compose
+   -b      Build local images
+EOF
 
-cp practica_big_data_2019/resources/download_data.sh Dockerfiles/Mongo/download_data.sh
-cp practica_big_data_2019/resources/import_distances.sh Dockerfiles/Mongo/import_distances.sh
+}
 
+getRemote(){
+  sudo apt-get update
 
+  sudo apt-get install -y git
+  git clone https://github.com/ging/practica_big_data_2019
+
+  cp practica_big_data_2019/resources/download_data.sh Dockerfiles/Mongo/download_data.sh
+  cp practica_big_data_2019/resources/import_distances.sh Dockerfiles/Mongo/import_distances.sh
+}
+
+installDockerCompose(){}
+
+installDocker(){}
+
+buildImages(){}
+
+if [ "$#" -eq 0 ]
+then
+  getRemote
+else
+  while getopts “hdcb” OPTION
+  do
+    case $OPTION in
+      h)
+        usage
+        exit 1
+        ;;
+      e)
+        installDocker
+        ;;
+      c)
+        installDockerCompose
+        ;;
+      b)
+        buildImages
+        ;;
+      ?)
+        usage
+        exit
+        ;;
+    esac
+  done
+fi
